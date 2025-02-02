e3811d7d535debbe7b179a5058e2c8a7abd8428d
kink2mg <viniciusvinihhh2003@gmail.com>
2025-02-02 15:45:07 -0300

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,8 +6,10 @@ const Navbar = () => {
   const whatsappNumber = "5538998622897"; // Número do WhatsApp
   const whatsappMessage = "Olá! Gostaria de saber mais sobre os planos.";
   const whatsappUrl = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(whatsappMessage)}`;
-  
-  const facebookUrl = "https://www.facebook.com/seuPerfilOuPagina"; // Link do Facebook
+
+  const facebookUser = "seuPerfilOuPagina"; // Nome de usuário do Facebook
+  const facebookUrl = `https://www.facebook.com/${facebookUser}`; // Link do Facebook
+
   const instagramUser = "seuPerfilInstagram"; // Nome de usuário do Instagram
   const instagramUrl = `https://www.instagram.com/${instagramUser}`; // Link do Instagram
 
