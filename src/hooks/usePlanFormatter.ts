8a75206d76c581151a8eb68b9e3f072a698be8d8
kink2mg <viniciusvinihhh2003@gmail.com>
2025-02-03 12:33:28 -0300

    Update usePlanFormatter.ts

--- src/hooks/usePlanFormatter.ts
+++ src/hooks/usePlanFormatter.ts
@@ -7,10 +7,8 @@ export const usePlanFormatter = () => {
   };
 
   const formatSales = (sales: number) => {
-    if (sales >= 1000000) {
-      return `${(sales / 1000000).toFixed(1).replace('.', ',')}M vendas`;
-    } else if (sales >= 1000) {
-      return `${(sales / 1000).toFixed(1).replace('.', ',')}k vendas`;
+    if (sales >= 1000) {
+      return `${(sales/1000).toFixed(1).replace('.', ',')}k vendas`;
     }
     return `${sales} vendas`;
   };
